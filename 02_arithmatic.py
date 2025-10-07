ad3567e6d61a8c10ef2e67ffb1622aab7d311167
Hasibul Hasan Khan <hasibulhasankhan281@gmail.com>
2025-10-07 15:31:59 +0600

    .

--- 02_arithmatic.py
+++ 02_arithmatic.py
@@ -2,3 +2,33 @@ import numpy as np
 
 ## Scalar arithmatic :
 
+arr = np.array([2, 4, 7])
+
+# print(arr + 1)
+# print(arr - 2)
+# print(arr * 3)
+# print(arr / 4)
+# print(arr ** 5)
+
+
+## Vectorized math func :
+
+# print(np.sqrt(arr))
+# print(np.round(arr))
+# print(np.floor(arr))
+# print(np.ceil(arr))
+# print(np.pi)
+
+## exercise :
+
+# print(np.pi * arr ** 2) 
+
+## Comparison operator :
+
+# print(arr == 2)
+# print(arr > 2)
+
+# arr[arr < 3] = 0
+# print(arr)
+
+
